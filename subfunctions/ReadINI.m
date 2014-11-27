7a6fc4c76a683d3d58ffcfa2e614c5b3bd5d4228
lillefrog <Christian_Brandt@yahoo.com>
2014-11-27 09:50:44 +0000

    Added check
    Check that the file exist before opening it

--- subfunctions/ReadINI.m
+++ subfunctions/ReadINI.m
@@ -5,34 +5,51 @@ function values = ReadINI(fName)
 % a line that starts with [ is a section head and must have a ] somewhere
 % any line that is not a comment or section must be a key and contain a =
 
-section = 'dafault'; % if there is no section name this will be used
+section = 'default'; % if there is no section name this will be used
 
 fid = fopen(fName); % Open the file 
 
-tline = fgetl(fid); % get the first line of text
-
-% run trough all the lines in the file one at a time
-while ischar(tline)  
-    tline = strtrim(tline); % remove spaces in the biginning and end of line
-    if length(tline)>1 % check that the line contains anything
-        switch tline(1); % look at the first caracter in the line
-            case '[' % this is a section
-                pos = strfind(tline,']');
-                section = tline(2:pos(1)-1);
-                %disp(['section = ',tline(2:pos(1)-1)]);
-            case {';','#','\','/','*'} % this is a comment
-                %disp('comment');
-            otherwise % this is a key
-                pos = strfind(tline,'=');
-                if ~isempty(pos)
-                    % disp(['  key = ',tline(1:pos(1)-1), '  value = ',tline(pos(1)+1:end)]);
-                    values.(section).(tline(1:pos(1)-1)) = tline(pos(1)+1:end);
-                else
-                    warning(['Line in INI file(',fName,') corrupted : ',tline]); %#ok<WNTAG>
-                end
+if fid~=-1
+    values.INIfileFound = true;
+    tline = fgetl(fid); % get the first line of text
+    % run trough all the lines in the file one at a time
+    while ischar(tline)  
+        tline = strtrim(tline); % remove spaces in the biginning and end of line
+        if length(tline)>1 % check that the line contains anything
+            switch tline(1); % look at the first caracter in the line
+                case '[' % this is a section
+                    pos = strfind(tline,']'); % find end of section title
+                    section = tline(2:pos(1)-1);
+                    %disp(['section = ',tline(2:pos(1)-1)]);
+                case {';','#','\','/','*'} % this is a comment
+                    %disp('comment');
+                otherwise % this is a key
+                    pos = strfind(tline,'=');
+                    if ~isempty(pos)   
+                        key = tline(pos(1)+1:end);
+                        out = convertToNumber(key);
+                        values.(section).(tline(1:pos(1)-1)) = out;
+                    else
+                        warning(['Line in INI file(',fName,') corrupted : ',tline]); %#ok<WNTAG>
+                    end
+            end
         end
+        tline = fgetl(fid); % get a new line of text
     end
-    tline = fgetl(fid); % get a new line of text
+    fclose(fid); % close file
+else
+    values.INIfileFound = false;
 end
 
-fclose(fid); % close file
+function out = convertToNumber(key)
+% converts a key to a number if possible, if not it will just keep the
+% string
+
+number = str2double(key);
+if isnan(number)
+    out = key;
+else
+    out = number;
+end
+
+
