b08f62261d589652284f0177568bc6e9755714a3
lillefrog <Christian_Brandt@yahoo.com>
2014-12-08 17:27:58 +0000

    Added default settings to Analyze_MsaccData
    It now has a set of default settings that will be overwritten if you
    specify someting else.

--- Analyze_MsaccData.m
+++ Analyze_MsaccData.m
@@ -1,13 +1,28 @@
-function [outData] = Analyze_MsaccData(spikeFileName,selectedCell)
+function [resultData] = Analyze_MsaccData(spikeFileName,selectedCell,setup)
 
 % Ask for the spike filename if it is not given
  if nargin<1 || isempty(spikeFileName) || ~exist(spikeFileName,'file');
     [fileName,filePath] = uigetfile('*.*','open a spike data file','MultiSelect','off'); 
     spikeFileName = fullfile(filePath,fileName);
  end
+ 
+ close all
 
 %% settings
-SHOWPLOTS = true;
+
+ % default setup
+ defaultSetup.showfig = true;
+ defaultSetup.saveFigPath = 'E:\temp\';
+ defaultSetup.saveFileName = 'E:\temp\test.mat';
+
+ if nargin<3 || ~exist(setup,'var')
+     % if no setup is supplied use the default
+    setup = defaultSetup; 
+ else
+     % if there is a setup use defaultSetup to fill out any missing fields
+    setup = CombineStructures(setup,defaultSetup);
+ end
+
  
 %% Read data 
 
@@ -88,14 +103,20 @@ clear isCorrect validTrials allData
 %%
 
 %AlignString = 'NLX_SUBJECT_START'; % fixation
-AlignString = 'NLX_STIM_ON';
-%AlignString = 'NLX_SACCADE_START';
-%AlignString = 'NLX_TRIAL_START';
+
+
+% AlignString = 'NLX_STIM_ON';
+% xLimits = [-200 1000]; % range around align point to plot
+
+AlignString = 'NLX_SACCADE_START';
+xLimits = [-300 300]; % range around align point to plot
+% 
+% AlignString = 'NLX_TRIAL_START';
+% xLimits = [-100 1000]; % range around align point to plot
+
 alignEvent = NLX_event2num(AlignString);
+timeArray=( (xLimits(1)-100) : (xLimits(2)+100) ); % range to calculate data in
 
-
-timeArray=(-1000:2000); % range to calculate data in
-xLimits = [-1000 2000]; % range around align point to plot
 subPlotWidth = 0.18;
 subPlotHight = 0.15;
 nrDirections = 9;
@@ -117,15 +138,19 @@ for i=1:nrDirections
  posSubplot{i} = [0.5+(positionTarget/scaleFactor)-[subPlotWidth/2 subPlotHight/2] subPlotWidth subPlotHight];
  
  StimOnData = CalculateSpikeData(tempData,[0 500],NLX_event2num('NLX_STIM_ON'));
- SaccOnData = CalculateSpikeData(tempData,[0 500],NLX_event2num('NLX_SACCADE_START'));
+ SaccOnDataA = CalculateSpikeData(tempData,[-200 0],NLX_event2num('NLX_SACCADE_START'));
+ SaccOnDataB = CalculateSpikeData(tempData,[-100 100],NLX_event2num('NLX_SACCADE_START'));
+ SaccOnDataC = CalculateSpikeData(tempData,[0 200],NLX_event2num('NLX_SACCADE_START'));
  pos(i,:) = positionTarget;         %#ok<AGROW>
  StimOn{i} = StimOnData.nrSpikes;   %#ok<AGROW>
- SaccOn{i} = SaccOnData.nrSpikes;   %#ok<AGROW>
+ SaccOnA{i} = SaccOnDataA.nrSpikes;   %#ok<AGROW>
+ SaccOnB{i} = SaccOnDataB.nrSpikes;   %#ok<AGROW>
+ SaccOnC{i} = SaccOnDataC.nrSpikes;   %#ok<AGROW>
 end
 
 
 
-if SHOWPLOTS
+if setup.showfig
     % plot the histograms for all directions
     for i=1:nrDirections
         subplot('position',posSubplot{i}); % Make a subplot at a definded position
@@ -139,31 +164,56 @@ if SHOWPLOTS
     % spiff up the main figure
     figure(mainFig); % select the figur
     [~, name, ext] = fileparts(resultData.cortexFilename); % get filename for title
-    figureTitle = [name,ext,' Cell=',num2str(resultData.Cell), '  Align to: ', AlignString];
+    figureTitle = [name,ext,' Cell=',num2str(resultData.Cell), '  Align to: ', AlignString,' SpikeWidth: ',num2str(spkWidth.peakTrough,3)];
     axes('Position',[0 0 1 1],'Xlim',[0 1],'Ylim',[0 1],'Box','off','Visible','off','Units','normalized', 'clipping' , 'off'); % make invisible axis
-
-    %text(0.5,0.35,'SaccadeOnset','Color',[.2 .2 1]);
-    %text(0.5,0.33,'StimOnset','Color',[1 .2 .2]);
     text(0.5, 0.97,figureTitle,'VerticalAlignment', 'top','HorizontalAlignment', 'center','Interpreter', 'none'); % print title
     line([0.5 0.5],[0.49 0.51],'Color','k'); % draw the cross in the middle
     line([0.49 0.51],[0.5 0.5],'Color','k'); % draw the cross in the middle
 end
 
 % draw polar plots
-saccOnDirec = PlotPolarData(SaccOn,pos,[.2 .2 1],SHOWPLOTS);
-stimOnDirec = PlotPolarData(StimOn,pos,[1 .2 .2],SHOWPLOTS);
+% saccOnDirec = PlotPolarData(SaccOn,pos,[.2 .2 1],setup.showfig);
+saccOnDirec = PlotPolarData(SaccOnA,pos,[1 .2 .2],setup.showfig);
+SaccOnDirec = PlotPolarData(SaccOnB,pos,[.2 1 .2],setup.showfig);
+stimOnDirec = PlotPolarData(SaccOnC,pos,[.2 .2 1],setup.showfig);
 
-if SHOWPLOTS
+if setup.showfig
     text(0.4,0.35, ['SaccadeOnset pVal: ',num2str(saccOnDirec.bootVlengthPval)], 'Color',[.2 .2 1]);
     text(0.4,0.33, ['StimOnset    pVal: ',num2str(stimOnDirec.bootVlengthPval)], 'Color',[1 .2 .2]);
+
+    if exist(setup.saveFigPath,'dir') % save the figure 
+        figFileName = [setup.saveFigPath,'\',name,ext,'_',num2str(resultData.Cell),'.png'];
+        set(mainFig, 'PaperUnits', 'inches');
+        set(mainFig, 'PaperPosition', [0 0 8 8]);
+        print(mainFig,'-dpng','-r300',figFileName);
+        disp(['Figur Saved as: ',figFileName]);
+        %close(mainFig);
+    end
 end
 
 
 hold off
 
-outData.saccOnDirec = saccOnDirec;
-outData.stimOnDirec = stimOnDirec;
+%% select and save the output data
+resultData.saccOnDirec = saccOnDirec;
+%resultData.stimOnDirec = stimOnDirec;
+
+% save the data to a mat file
+if (length(setup.saveFileName)>2)    
+    myArray{1} = resultData;
+    if exist(setup.saveFileName,'file')
+        % append the data
+        load(setup.saveFileName); 
+        myPos = length(myArray)+1;
+        myArray{myPos} = resultData; %#ok<NASGU>
+        save(setup.saveFileName, 'myArray');
+    else
+        % save the data in a new file
+        myArray{1} = resultData; %#ok<NASGU>
+        save(setup.saveFileName, 'myArray');
+    end
+end
  
  
- 
+
 
