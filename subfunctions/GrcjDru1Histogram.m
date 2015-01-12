b6409dde26f1a1642c8dea2da7df7dcf86262738
lillefrog <Christian_Brandt@yahoo.com>
2015-01-12 18:22:51 +0000

    Prepare histogram for plotting
    This function is now setting the linewidth and colors directly instead
    of using the defaults in the plotting function

--- subfunctions/GrcjDru1Histogram.m
+++ subfunctions/GrcjDru1Histogram.m
@@ -4,7 +4,7 @@ function plotData = GrcjDru1Histogram(data,timeArray,alignEvent)
 %
 % Input:
 %  data = data from the AlignCtxAndNlxData function.
-%  timearray = the array we vant ot calculate histograms over.
+%  timearray = the array we want to calculate histograms over.
 %  alignEvent = The nlx event we want to align the spikes to.
 %
 % Output:
@@ -17,6 +17,19 @@ DataNoDrug = data( ~[data.drug]' );
 [plot_DataNoDrug] = CalculateSpikeHistogram(DataNoDrug,timeArray,alignEvent);
 
 plot_DataDrug.name = 'drug';
-plot_DataNoDrug.name = 'no drug';
+plot_DataDrug.lineWidth = 2;
+plot_DataDrug.spikeColor = [0 0 0];
+plot_DataDrug.histColor = [0 0 0];
+
+plot_DataNoDrug.name = 'control';
+plot_DataNoDrug.lineWidth = 1;
+plot_DataNoDrug.spikeColor = [0.3 0.3 0.3];
+plot_DataNoDrug.histColor = [0.3 0.3 0.3];
+
+% Find the maximum amplitude for both blots and set that as the max for
+% both
+maxHist = max([plot_DataDrug.maxHist , plot_DataNoDrug.maxHist]) ;
+plot_DataDrug.maxHist = maxHist;
+plot_DataNoDrug.maxHist = maxHist;
 
 plotData = [plot_DataNoDrug, plot_DataDrug];
