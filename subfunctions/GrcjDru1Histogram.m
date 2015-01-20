94718dd0393929e7b09f28f5bb9a4d6d6224bd42
lillefrog <Christian_Brandt@yahoo.com>
2015-01-20 17:40:41 +0000

    Grcjdru1 Histogramme
    Add Offset

--- subfunctions/GrcjDru1Histogram.m
+++ subfunctions/GrcjDru1Histogram.m
@@ -1,4 +1,4 @@
-function plotData = GrcjDru1Histogram(data,timeArray,alignEvent)
+function plotData = GrcjDru1Histogram(data,timeArray,alignEvent,offset)
 % Specialist function for plotting drug vs no drug plots. this is just a
 % wrapper for the more basic CalculateSpikeHistogram.
 %
@@ -10,21 +10,42 @@ function plotData = GrcjDru1Histogram(data,timeArray,alignEvent)
 % Output:
 %  plotData = data for sending to the PlotSpikeHistogram function
 
+ if nargin<4 || ~exist('offset','var')
+     offsetNoDrug = 0;
+     offsetDrug = 0;
+ else
+     if strcmp(class(offset),'double')
+         if length(offset)==2
+             offsetNoDrug = offset(1);
+             offsetDrug = offset(2);
+         else
+             offsetNoDrug = offset(1);
+             offsetDrug = offset(1);
+         end
+     end
+     %elseif strcmp(class(offset),'struct') % in case I want to expand the function       
+ end
+disp(offset);
+ 
 DataDrug   = data(  [data.drug]' );
 DataNoDrug = data( ~[data.drug]' );
 
-[plot_DataDrug] = CalculateSpikeHistogram(DataDrug,timeArray,alignEvent);
+[plot_DataDrug] = CalculateSpikeHistogram(DataDrug,timeArray,alignEvent); < Add here
 [plot_DataNoDrug] = CalculateSpikeHistogram(DataNoDrug,timeArray,alignEvent);
 
 plot_DataDrug.name = 'drug';
 plot_DataDrug.lineWidth = 2;
 plot_DataDrug.spikeColor = [0 0 0];
 plot_DataDrug.histColor = [0 0 0];
+plot_DataDrug.yHistogram = plot_DataDrug.yHistogram - offset;
+
 
 plot_DataNoDrug.name = 'control';
 plot_DataNoDrug.lineWidth = 1;
 plot_DataNoDrug.spikeColor = [0.3 0.3 0.3];
 plot_DataNoDrug.histColor = [0.3 0.3 0.3];
+plot_DataNoDrug.yHistogram = plot_DataNoDrug.yHistogram - offset;
+
 
 % Find the maximum amplitude for both blots and set that as the max for
 % both
