90ca56eae66725c86a85d6d9b9ecf4b015ca18fd
lillefrog <Christian_Brandt@yahoo.com>
2015-01-26 17:14:50 +0000

    Add histogram plots together
    New version that is much more general. it is now less depended on the
    excat structure of the data. You still have to be careful though.

--- subfunctions/SumHistPlots.m
+++ subfunctions/SumHistPlots.m
@@ -16,63 +16,42 @@ function outData = SumHistPlots(plotDataArray)
 % change. This means that those values might be wrong
 outData = plotDataArray{1};
 
-% go trough figures from 1 to 9 and find all the plot data and sum all
-% the normalized values together. And then divide with the number of files.
 for i=1:9     % go trough all possible figures (only 9 so far)
-    figName = ['fig',num2str(i)];
+    figName = ['fig',num2str(i)]; % generate a possible figure name
     if isfield(outData, figName); % check if that figure exist
         names = fieldnames(outData.(figName).plotdata); % get names of all subfilds
         for k = 1:length(names)  % go trough all subfields    
             % Use the first cell as a template for the parameters we wont
             % change. This means that those values might be wrong
+
+            % get initial sizes to use for initializing array
             tempSumData = plotDataArray{1}.(figName).plotdata.(names{k});
+
+            % initialize array
+            tempDataArray = zeros(length(plotDataArray),length(tempSumData),length(tempSumData(1).yHistogram) );
             
-            % add this fields together for each cell
-            for j=2:length(plotDataArray)
-                tempData = plotDataArray{j}.(figName).plotdata.(names{k});
-                tempSumData = addPlotData(tempSumData,tempData);
-            end
+            % extract the data from the structs and into an array
+            for j=1:length(plotDataArray) 
+                TempplotData = plotDataArray{j}.(figName).plotdata.(names{k});
+                for m=1:length(TempplotData)
+                    tempDataArray(j,m,:) = TempplotData(m).yHistogram;
+                end
+            end           
             
-            %divide with number of files
-            for m = 1:length(tempSumData)
-                tempSumData(m).yHistogram = tempSumData(m).yHistogram / tempSumData(m).sumOffiles;
-                tempSumData(m).sumOffiles = 1;
-                tempSumData(m).isNormalized = true;
-            end
+            % calculate mean and STD from our array
+            meanDataArray = nanmean(tempDataArray,1);
+            stdDataArray = nanstd(tempDataArray,1) / sqrt(size(tempDataArray,1));
             
+            
+            %transfer the data back into a structure
+            for m = 1:size(meanDataArray,2)
+                tempSumData(m).yHistogram = (squeeze(meanDataArray(1,m,:)))';
+                tempSumData(m).yHistogramSEM = (squeeze(stdDataArray(1,m,:)))';
+            end           
+
             % transfer the data to the output variable
             outData.(figName).plotdata.(names{k}) = tempSumData;
+            
         end
     end
 end
-
-function sum = addPlotData(hist1,hist2)
-% adding together two plotData structures
-sum = hist1; % initialize
-
-% check if the sum of files field exist, it keeps track of how many files
-% you have combined together. This is needed for normalizing
-fieldExist1 = isfield(hist1(1),'sumOffiles');
-fieldExist2 = isfield(hist2(1),'sumOffiles');
-
-for j=1:length(hist1);
-    if hist1(j).name == hist2(j).name % check that the fields contain the same data
-        normalizedHist1 = hist1(j).yHistogram / hist1(j).maxHist; % normalize
-        normalizedHist2 = hist2(j).yHistogram / hist2(j).maxHist; % normalize
-        sum(j).yHistogram =  normalizedHist1 + normalizedHist2; % add the data
-        sum(j).maxHist = 1; % if we don't do the we will normalize the same data again and again
-    else
-        error('The files being summed does not contain the same data');
-    end
-
-    % keep track of how many files are added together 
-    if ~fieldExist1
-        hist1(j).sumOffiles = 1;
-    end
-
-    if ~fieldExist2
-        hist2(j).sumOffiles = 1;
-    end
-
-    sum(j).sumOffiles = hist2(j).sumOffiles + hist1(j).sumOffiles;     
-end 
